f191e5227e5b287989fdae6ba9b028e34448070e
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-02-25 11:47:34 +0100

    fix: run on 0.0.0.0 and logging fix

--- main.go
+++ main.go
@@ -25,19 +25,17 @@ func validToken(token string) bool {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
-	
-	fmt.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-	
+
 	status := http.StatusUnauthorized
-	
+
 	if len(authHeader) != 2 {
 		status = http.StatusBadRequest
 	} else if validToken(authHeader[1]) {
 		status = http.StatusOK
 	}
-	
-	fmt.Printf("%s %s %s %s\n", r.RemoteAddr, r.Method, r.URL, status)
-	
+
+	fmt.Printf("%s %s %s %d\n", r.RemoteAddr, r.Method, r.URL, status)
+
 	w.WriteHeader(status)
 }
 
@@ -45,8 +43,8 @@ func main() {
 	httpPort := 8087
 	loadToken()
 	http.HandleFunc("/", handler)
-	
+
 	fmt.Printf("listening on %v\n", httpPort)
-	
-	http.ListenAndServe(fmt.Sprintf(":%v", httpPort), nil)
+
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%v", httpPort), nil)
 }
